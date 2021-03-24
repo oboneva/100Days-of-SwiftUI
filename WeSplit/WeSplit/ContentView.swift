715bb106e28cca544f2fbd07b9892878da350e8e
Ognyanka Boneva <ogi.boneva@gmail.com>
2021-03-25 00:07:07 +0200

    Project 1, part two
    Implement basic functionality for WeSplit

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,25 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
-    @State private var selectedStudent = 0
-    let students = ["Harry", "Ron", "Hermione"]
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentageIndex = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentageIndex])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = tipValue + orderAmount
+        
+        return grandTotal / peopleCount
+    }
     
     var body: some View {
         NavigationView {
             Form {
-                TextField("Enter your name: ", text: $name)
-                Text("Your name is \(name)")
+                Section {
+                    TextField("Amount ", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                }
                 
-                VStack {
-                    Picker("Select your student", selection: $selectedStudent) {
-                        ForEach(0..<students.count) {
-                            Text(self.students[$0])
-                        }
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2 ..< 100) {
+                        Text("\($0) people")
                     }
                 }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentageIndex) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\($0)%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("\(totalPerPerson, specifier: "%.2f")")
+                }
             }
-            .navigationBarTitle("SwiftUI")
+            .navigationBarTitle("WeSplit")
         }
     }
 }
