11e2dc66735ab6279abfb4abfd3a9576c6e8ea48
Ognyanka Boneva <ogi.boneva@gmail.com>
2021-04-06 00:35:16 +0300

    Project 5, part 1

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ognyanka Boneva on 6.04.21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
