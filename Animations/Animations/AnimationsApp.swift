5155065ba9f7b0ffff812437a5c1ee788b6318a8
Ognyanka Boneva <ogi.boneva@gmail.com>
2021-04-11 22:03:20 +0300

    Project 6, part 1
    Playing around with implicit animations, explicit animations, binding animations, and more.

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ognyanka Boneva on 11.04.21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
