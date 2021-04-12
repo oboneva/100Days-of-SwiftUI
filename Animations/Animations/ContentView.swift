49923236508d5d49aaa461c64bf6a262d778bcad
Ognyanka Boneva <ogi.boneva@gmail.com>
2021-04-12 13:25:32 +0300

    Project 6, part 2
    Playing around with multiple animations, gesture animations, transitions, and more.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+//    @State private var animationAmount = 0.0
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var isShowingRed = false
     
     var body: some View {
 //        Button("Tap me") {
@@ -69,16 +75,102 @@ struct ContentView: View {
 //            .clipShape(Circle())
 //            .scaleEffect(animationAmount)
 //        }
-        Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
+//        Button("Tap Me") {
+//            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+//                self.animationAmount += 360
+//            }
+//        }
+//        .padding(50)
+//        .background(Color.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
+//        Button("Tap Me") {
+//            self.enabled.toggle()
+//        }
+//        .frame(width: 200, height: 200)    
+//        .background(enabled ? Color.blue : Color.red)
+//        .foregroundColor(.white)
+//        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+//        .animation(.default)
+        
+//        Button("Tap Me") {
+//            self.enabled.toggle()
+//        }
+//        .frame(width: 200, height: 200)
+//        .background(enabled ? Color.blue : Color.red)
+////        .animation(.default)
+//        .animation(nil)
+//        .foregroundColor(.white)
+//        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+//        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        
+//        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//            .offset(dragAmount)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { self.dragAmount = $0.translation }
+//                    .onEnded { _ in
+//                        withAnimation(.spring()) {
+//                            self.dragAmount = .zero
+//                        }
+//                    }
+//            )
+//            .animation(.spring())
+        
+//        HStack(spacing: 0) {
+//            ForEach(0..<letters.count) { num in
+//                Text(String(self.letters[num]))
+//                    .padding(5)
+//                    .font(.title)
+//                    .background( enabled ? Color.blue : Color.red)
+//                    .offset(dragAmount)
+//                    .animation(Animation.default.delay(Double(num) / 20))
+//            }
+//        }
+//        .gesture(
+//            DragGesture()
+//                .onChanged { self.dragAmount = $0.translation }
+//                .onEnded { 
+//                    _ in
+//                    self.dragAmount = .zero
+//                    self.enabled.toggle()
+//                }
+//        )
+        
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+                  identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
     }
 }
 
