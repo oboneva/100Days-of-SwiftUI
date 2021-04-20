b65604e4847c9a1f0dc6b3aa6af8a94ea010d028
Ognyanka Boneva <ogi.boneva@gmail.com>
2021-04-20 18:59:12 +0300

    Project 9, part 2
    Playing around with CGAffineTransform, ImagePaint, drawingGroup(), and more.

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,6 +7,55 @@
 
 import SwiftUI
 
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct Flower: Shape {
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
+    }
+}
+
 struct Arc: Shape, InsettableShape {
     var startAngle: Angle
     var endAngle: Angle
@@ -45,6 +94,10 @@ struct Triangle: Shape {
 }
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
+    
     var body: some View {
 //        Path { path in
 //            path.move(to: CGPoint(x: 200, y: 100))
@@ -68,8 +121,33 @@ struct ContentView: View {
 //            .stroke(Color.blue, lineWidth: 40)
 //            .strokeBorder(Color.blue, lineWidth: 40)
         
-        Arc(startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
-            .strokeBorder(Color.blue, lineWidth: 40)
+//        Arc(startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+//            .strokeBorder(Color.blue, lineWidth: 40)
+        
+//        VStack {
+//            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+//                .stroke(Color.red, lineWidth: 1)
+//                .fill(Color.red, style: FillStyle(eoFill: true))
+//            
+//            Text("Offset")
+//            Slider(value: $petalOffset, in: -40...40)
+//                .padding([.horizontal, .bottom])
+//            
+//            Text("Width")
+//            Slider(value: $petalWidth, in: 0...100)
+//                .padding([.horizontal])
+//        }
+        
+//        Text("Hello World")
+//            .frame(width: 300, height: 300)
+//            .border(ImagePaint(image: Image("Screenshot"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+        
+        VStack {
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
     }
 }
 
